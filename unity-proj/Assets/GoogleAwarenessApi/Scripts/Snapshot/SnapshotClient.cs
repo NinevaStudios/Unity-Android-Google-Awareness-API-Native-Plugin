79dc74c082dd31a32269e6d96f17b33728b5b92f
Taras Leskiv <tleskiv@transfollow.org>
2018-08-16 09:29:11 +0300

    added jni toolkit

--- unity-proj/Assets/GoogleAwarenessApi/Scripts/Snapshot/SnapshotClient.cs
+++ unity-proj/Assets/GoogleAwarenessApi/Scripts/Snapshot/SnapshotClient.cs
@@ -9,38 +9,48 @@ namespace NinevaStudios.AwarenessApi
 	[PublicAPI]
 	public static class SnapshotClient
 	{
+		static AndroidJavaObject _client;
+
 		public static void GetBeaconState(Action<BeaconStateResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
 		}
 
 		public static void GetDetectedActivity(Action<DetectedActivityResponse> onSuccess)
 		{
+			CreateClientLazy();
 		}
 
 		public static void GetHeadphoneState(Action<HeadphoneStateResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
 		}
-		
+
 		public static void GetLocation(Action<LocationResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
 		}
-		
+
 		public static void GetPlaces(Action<PlacesResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
 		}
-		
+
 		public static void GetTimeIntervals(Action<TimeIntervalsResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
 		}
-		
+
 		public static void GetWeather(Action<WeatherResponse> onSuccess)
 		{
-			
+			CreateClientLazy();
+		}
+
+		static void CreateClientLazy()
+		{
+			if (_client == null)
+			{
+			}
 		}
 	}
 }

25949598014063b4815bed94f62214c620185d38
Taras Leskiv <tleskiv@transfollow.org>
2018-08-29 12:11:56 +0300

    beacon fence

--- unity-proj/Assets/GoogleAwarenessApi/Scripts/Fence/BeaconFence.cs
+++ unity-proj/Assets/GoogleAwarenessApi/Scripts/Fence/BeaconFence.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 
 namespace NinevaStudios.AwarenessApi
 {
@@ -12,6 +14,81 @@ namespace NinevaStudios.AwarenessApi
 	[PublicAPI]
 	public class BeaconFence
 	{
-		
+		const string BeaconFenceClass = "com.google.android.gms.awareness.fence.BeaconFence";
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is found.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Found(IEnumerable<BeaconState.TypeFilter> beaconTypes)
+		{
+			var filters = beaconTypes.ToList().ToJavaList(x => x.AJO);
+			return new AwarenessFence(BeaconFenceClass.AJCCallStaticOnceAJO("found", filters));
+		}
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is found.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Found(params BeaconState.TypeFilter[] beaconTypes)
+		{
+			return Found(beaconTypes.ToList());
+		}
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is lost.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Lost(IEnumerable<BeaconState.TypeFilter> beaconTypes)
+		{
+			var filters = beaconTypes.ToList().ToJavaList(x => x.AJO);
+			return new AwarenessFence(BeaconFenceClass.AJCCallStaticOnceAJO("lost", filters));
+		}
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is lost.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Lost(params BeaconState.TypeFilter[] beaconTypes)
+		{
+			return Lost(beaconTypes.ToList());
+		}
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is found but not lost.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Near(IEnumerable<BeaconState.TypeFilter> beaconTypes)
+		{
+			var filters = beaconTypes.ToList().ToJavaList(x => x.AJO);
+			return new AwarenessFence(BeaconFenceClass.AJCCallStaticOnceAJO("near", filters));
+		}
+
+		/// <summary>
+		/// This fence is momentarily TRUE (about 5 seconds) when a beacon with the specified types is found but not lost.
+		///
+		/// To use this API, your app must declare the android.permission.ACCESS_FINE_LOCATION permission in AndroidManifest.xml, and the user must provide consent at runtime.
+		/// </summary>
+		/// <param name="beaconTypes"><see cref="BeaconState.TypeFilter"/> non-empty beacon types listing to scan for.</param>
+		/// <returns><see cref="AwarenessFence"/></returns>
+		public static AwarenessFence Near(params BeaconState.TypeFilter[] beaconTypes)
+		{
+			return Near(beaconTypes.ToList());
+		}
 	}
 }
